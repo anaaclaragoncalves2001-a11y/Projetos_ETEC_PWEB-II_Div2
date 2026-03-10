146fae8a62f91a3334138982d6d63a0b6b203b6e
ricarrdoszanata <ricardoszanata@gmail.com>
2026-03-10 12:36:11 -0300

    acerto do card e utilização da condição while

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ $titulo = "Notebook VAIO FE16";
 $resumo = "Notebook VAIO FE16 AMD® Ryzen 7-5825U Linux 8GB RAM 512GB SSD 16' IPS WUXGA Antirreflexo - Cinza Grafite";
 $valor = 3399.98;
 $quantidade = 5;
+$i = 1;
 ?>
 <!doctype html>
 <html lang="pt-BR">
@@ -134,16 +135,20 @@ $quantidade = 5;
       </button>
     </div>
     <div class="row">
+      <?php while($i<=10){ ?>
       <div class="col mt-2">
         <div class="card" style="width: 18rem;">
           <img src="<?php echo $imagem; ?>" class="card-img-top" alt="...">
           <div class="card-body">
             <h5 class="card-title"><?php echo $titulo; ?></h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+            <p class="card-text"><?php echo $resumo; ?></p>
+            <p class="text-danger">Valor:<b>R$ <?php echo $valor; ?></b></p>
+            <p class="text-primary">Estoque: <?php echo $quantidade; ?></p>
+            <a href="#" class="btn btn-primary">Ver Produto</a>
           </div>
         </div>
       </div>
+      <?php $i++; }//$i=$i+1?>
     </div>
   </main>
 
